982d687f5e5c01e87d71ada629e0d355dabeb34e
Regan Sarwas <regan_sarwas@nps.gov>
2018-04-30 08:20:32 -0800

    Remove files no longer in upstream

--- run.R
+++
@@ -1,8 +0,0 @@
-# script that install packages, run app
-if (!require("devtools"))
-  install.packages("devtools")
-devtools::install_github("rstudio/shiny")
-devtools::install_github("ctmm-initiative/ctmm")
-devtools::install_github("r-lib/crayon")
-# run app
-shiny::runGitHub('regan-sarwas/ctmm-webapp', host="0.0.0.0", port=7893)
